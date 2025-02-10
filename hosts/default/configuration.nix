5effbd50f269e4f3205a74cef9057e0f2aea6b81
JacobRosenlund <jakemrosenlund@gmail.com>
2025-02-10 13:40:17 -0700

    Build 2

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -7,35 +7,47 @@
       inputs.home-manager.nixosModules.default
     ];
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  nixpkgs.config.allowUnfree = true;
-
+  # Use systemd-boot EFI boot loader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Network
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
+  # Time zone
   time.timeZone = "America/Denver";
 
-  # Enable Hyprland
-  programs.hyprland.enable = true;
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable Bluetooth
-  hardware.bluetooth.enable = true;
-
   # Enable sound.
-  services.pipewire = {
-    pulse.enable = true;
+  # services.pipewire = {
+  #   pulse.enable = true;
+  # };
+  sound.enable = true;
+  hardware.pulseaudio = {
+    enable = true;
+    package = pkgs.pulseaudioFull;
+    extraConfig = ''
+      load-module module-switch-on-connect
+    '';
   };
+
+  # Bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+  services.blueman.enable = true;
+
+  # Syncthing goes here
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
 
+  # Session Variables
   environment.sessionVariables = {
     EDITOR = "nvim";
     DESKTOP = "$HOME/Desktop";
@@ -47,23 +59,11 @@
     VIDEOS = "$HOME/Videos";
   };
 
-  programs.fish.enable = true;
+  # Nixpkgs
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
 
-  users.users.jacobr = {
-    isNormalUser = true;
-    description = "Jacob Rosenlund";
-    extraGroups = [ "wheel" ];
-    packages = with pkgs; [ 
-    ];
-  };
-
-  home-manager = {
-    extraSpecialArgs = { inherit inputs; };
-    users = {
-      "jacobr" = import ./home.nix;
-    };
-  };
-
+  # System Packages
   environment.systemPackages = with pkgs; [
     kitty
     fish
@@ -74,22 +74,49 @@
     eza
     viddy
     git
+    lazygit
     gh
     wget
     stow
+    nerdfetch
     kanata
   ];
 
-  # services.greetd = {
-  #   enable = true;
-  #   settings = rec {
-  #     initial_session = {
-  #       command = "${pkgs.hyprland}/bin/hyprland";
-  #       user = "jacobr";
-  #     };
-  #     default_session = initial_session;
-  #   };
-  # };
+  # Enable Hyprland
+  programs.hyprland.enable = true;
+
+  # Enable Fish
+  programs.fish.enable = true;
+  users.defaultUserShell = pkgs.fish;
+
+  # Make Users
+  users.users.jacobr = {
+    isNormalUser = true;
+    description = "Jacob Rosenlund";
+    extraGroups = [ "wheel" ];
+    packages = with pkgs; [ 
+    ];
+  };
+
+  # Home manager
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "jacobr" = import ./home.nix;
+    };
+  };
+
+  # Launch to Hyprland
+  services.greetd = {
+    enable = true;
+    settings = rec {
+      initial_session = {
+        command = "${pkgs.hyprland}/bin/hyprland";
+        user = "jacobr";
+      };
+      default_session = initial_session;
+    };
+  };
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
