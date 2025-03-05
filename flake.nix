be19c299ab5d2c964c8e42fd200a84cac6e8d17c
JacobRosenlund <jakemrosenlund@gmail.com>
2025-03-05 15:56:33 -0700

    Updated flake.nix

--- flake.nix
+++ flake.nix
@@ -17,24 +17,24 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
-	config.allowUnfree = true;
-	config.allowUnsupportedSystem = true;
-	config.permittedInsecurePackages = [
-	  "openssl-1.1.1w"
-	];
+	      config.allowUnfree = true;
       };
     in {
       nixosConfigurations = {
         default = nixpkgs.lib.nixosSystem {
           specialArgs = {inherit inputs;};
-	  inherit system;
-	  inherit pkgs;
+	        inherit system;
+	        inherit pkgs;
           modules = [
             ./hosts/default/configuration.nix
             inputs.home-manager.nixosModules.default
             inputs.stylix.nixosModules.stylix
+
+            # Modules
+              ./modules/nixos/enable-hardware.nix
           ];
         };
       };
   };
 }
+
