7b7ad2d3ab55cb0276d87b6d9d6d1c7667078406
JacobRosenlund <jakemrosenlund@gmail.com>
2025-09-09 22:35:05 -0600

    Added new packages

--- hosts/default/user-packages.nix
+++ hosts/default/user-packages.nix
@@ -4,11 +4,11 @@
   imports = [
     (modulePath + "/packages/user-packages/creative.nix")
     (modulePath + "/packages/user-packages/communication.nix")
-    (modulePath + "/packages/user-packages/file-manager.nix")
     (modulePath + "/packages/user-packages/fonts.nix")
+    (modulePath + "/packages/user-packages/games.nix")
     (modulePath + "/packages/user-packages/media.nix")
-    (modulePath + "/packages/user-packages/modeling.nix")
     (modulePath + "/packages/user-packages/password-manager.nix")
+    (modulePath + "/packages/user-packages/reader.nix")
     (modulePath + "/packages/user-packages/window-manager.nix")
   ];
 }
