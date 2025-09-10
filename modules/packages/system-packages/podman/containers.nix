73727b84e1cb6b6bf178bf438c41125cff612998
JacobRosenlund <jakemrosenlund@gmail.com>
2025-09-10 11:06:00 -0600

    Fixed container config

--- modules/packages/system-packages/podman/containers.nix
+++ modules/packages/system-packages/podman/containers.nix
@@ -3,8 +3,8 @@
 {
   virtualisation.oci-containers.backend = "podman";
   virtualisation.oci-containers.containers = {
-    container-name = {
-      image = "speedtest-tracker";
+    speedtest-tracker = {
+      image = "lscr.io/linuxserver/speedtest-tracker:latest";
       autoStart = true;
       ports = [ "127.0.0.1:8085:80" "0.0.0.0:8443:443" ];
     };
