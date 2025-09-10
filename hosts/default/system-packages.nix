d6d2cab17c4580039d3ca86cbd487d885a131cc1
JacobRosenlund <jakemrosenlund@gmail.com>
2025-09-09 22:34:55 -0600

    Added new packages and reformatted

--- hosts/default/system-packages.nix
+++ hosts/default/system-packages.nix
@@ -9,13 +9,19 @@
     (modulePath + "/packages/languages/lua.nix")
     (modulePath + "/packages/languages/python.nix")
     (modulePath + "/packages/languages/sql.nix")
-    (modulePath + "/packages/system-packages/network.nix")
     (modulePath + "/packages/systemPackages/clipboard.nix")
-    (modulePath + "/packages/systemPackages/device-manage.nix")
+    (modulePath + "/packages/system-packages/device-manage.nix")
+    (modulePath + "/packages/system-packages/file-managers.nix")
+    (modulePath + "/packages/system-packages/ide.nix")
+    (modulePath + "/packages/system-packages/network.nix")
+    (modulePath + "/packages/system-packages/radio.nix")
+    (modulePath + "/packages/system-packages/rpi.nix")
+    (modulePath + "/packages/system-packages/system-build.nix")
+    (modulePath + "/packages/system-packages/system-health.nix")
+    (modulePath + "/packages/system-packages/torrent.nix")
   ];
 
   environment.systemPackages = with pkgs; [
-    gparted
     openssl
     
     libnotify               # Notification utilites
