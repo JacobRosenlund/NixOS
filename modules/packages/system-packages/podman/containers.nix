af286fe5cfa01efb45d818e047f8810c84b3e597
JacobRosenlund <jakemrosenlund@gmail.com>
2025-09-10 11:38:31 -0600

    Updated gid

--- modules/packages/system-packages/podman/containers.nix
+++ modules/packages/system-packages/podman/containers.nix
@@ -10,12 +10,12 @@
       volumes = [ "/var/lib/speedtest-tracker:/config" ];
       environment = {
         PUID = "1000";
-        PGID = "1000";
+        PGID = "100";
         APP_KEY = "base64:CnElB1TbfraB2hTFlaofnoAaFBMONsb5i1HxZKkaWSE=";
         DB_CONNECTION = "sqlite";
         TZ = "America/Denver";
         SPEEDTEST_SCHEDULE = "*/10 * * * *";
-        PUBLIC_DASHBOARD = true;
+        PUBLIC_DASHBOARD = "true";
       };
     };
   };
