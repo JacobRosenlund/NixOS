b0fe1c66c2c4b707f51a894397c5d89a2c748999
JacobRosenlund <jakemrosenlund@gmail.com>
2025-09-09 22:16:36 -0600

    added lua-language-server

--- modules/packages/languages/lua.nix
+++ modules/packages/languages/lua.nix
@@ -1,5 +1,5 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ lua5_1 luarocks ];
+  environment.systemPackages = with pkgs; [ lua-language-server lua5_1 luarocks ];
 }
