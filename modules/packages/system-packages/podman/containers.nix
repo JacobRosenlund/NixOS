5f439002a07c8288857fc6674f3a5a8347d31a15
JacobRosenlund <jakemrosenlund@gmail.com>
2025-09-10 11:25:08 -0600

    Updated containers

--- modules/packages/system-packages/podman/containers.nix
+++ modules/packages/system-packages/podman/containers.nix
@@ -7,6 +7,16 @@
       image = "lscr.io/linuxserver/speedtest-tracker:latest";
       autoStart = true;
       ports = [ "127.0.0.1:8085:80" "0.0.0.0:8443:443" ];
+      volumes = [ "/var/lib/speedtest-tracker:/config" ];
+      environment = {
+        PUID = "1000";
+        PGID = "1000";
+        APP_KEY = "base64:CnElB1TbfraB2hTFlaofnoAaFBMONsb5i1HxZKkaWSE=";
+        DB_CONNECTION = "sqlite";
+        TZ = "America/Denver";
+        SPEEDTEST_SCHEDULE = "*/10 * * * *";
+        PUBLIC_DASHBOARD = true;
+      };
     };
   };
 }
