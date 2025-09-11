6b7e46cb8e94c11ad822d9a61fa3fcd64edc059c
JacobRosenlund <jakemrosenlund@gmail.com>
2025-09-11 00:11:25 -0600

    Fixed avahi and updated ports for nginxproxymanager

--- modules/nixos/network/config.nix
+++ modules/nixos/network/config.nix
@@ -9,15 +9,20 @@
   # General Network
     networking.hostName = "nixos";            # This system's hostname
     networking.networkmanager.enable = true;  # System for configurng network
-    services.avahi.enable = true;             # mDNS Service
-    services.avahi.nssmdns4= true;            # Allows `.local` usage
+    services.avahi = {
+      enable = true;        # mDNS Service
+      nssmdns4 = true;      # Allows `.local` usage
+      openFirewall = true;  # Opens port for mDNS
+    };
+
 
   # Firewall options
     # Allowing:
+    # - 80 = nginx
     # - 8080 = Calibre
     # - 9090 = Minecraft
     # - 22 = SSH
     networking.firewall.enable = true;
-    networking.firewall.allowedTCPPorts = [ 8080 9090 22 ];
+    networking.firewall.allowedTCPPorts = [ 80 8080 9090 22 ];
     networking.firewall.allowedUDPPorts = [ 9090 ];
 }
