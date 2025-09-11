0f137bbd9fee03fb4d5cdb2319a997824326617d
JacobRosenlund <jakemrosenlund@gmail.com>
2025-09-10 18:40:28 -0600

    Added nginx-proxy-manager

--- modules/packages/system-packages/podman/containers.nix
+++ modules/packages/system-packages/podman/containers.nix
@@ -6,7 +6,7 @@
     speedtest-tracker = {
       image = "lscr.io/linuxserver/speedtest-tracker:latest";
       autoStart = true;
-      ports = [ "127.0.0.1:8085:80" "0.0.0.0:8443:443" ];
+      ports = [ "127.0.0.1:8085:80" ];
       volumes = [ "/var/lib/speedtest-tracker:/config" ];
       environment = {
         PUID = "1000";
@@ -17,6 +17,17 @@
         SPEEDTEST_SCHEDULE = "*/10 * * * *";
         PUBLIC_DASHBOARD = "true";
       };
+    };
+
+    nginx-proxy-manager = {
+      image = "jc21/nginx-proxy-manager:latest";
+      autoStart = true;
+      ports = [ "80:80" "81:81" "433:433" ];
+      volumes = [
+        "/var/lib/nginx-proxy-manager/data:/data"
+        "/var/lib/nginx-proxy-manager/letsencrypt:/etc/letsencrypt"
+        "/var/lib/nginx-proxy-manager/98-themepark-npm:/etc/cont-init.d/99-themepark"
+      ];
     };
   };
 }
