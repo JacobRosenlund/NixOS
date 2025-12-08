9339588abcea4b20c92fed3b26fb2db8d95291b8
JacobRosenlund <jakemrosenlund@gmail.com>
2025-12-08 03:38:31 -0700

    Added basic Espanso config

--- modules/packages/system-packages/espanso.nix
+++ modules/packages/system-packages/espanso.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
-  # environment.systemPackages = with pkgs; [ espanso ];
-  services.espanso.enable = true;
+  environment.systemPackages = with pkgs; [ espanso-wayland xorg.setxkbmap ];
+
+  users.users.jacobr.extraGroups = [ "input" ];
 }
