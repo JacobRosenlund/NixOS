9dde5c2a0b38df8723e6a793ba510dc6aa32002b
JacobRosenlund <jakemrosenlund@gmail.com>
2025-12-18 14:18:39 -0700

    Updated stylix

--- hosts/default/stylix.nix
+++ hosts/default/stylix.nix
@@ -1,13 +1,16 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
-  stylix.enable = true;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/helios.yaml";
-  stylix.cursor = {
-    package = pkgs.posy-cursors;
-    name = "Posy_Cursor_Mono_Black";
-    size = 24;
+  stylix = {
+    enable = true;
+    targets.grub.enable = false;
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/helios.yaml";
+    cursor = {
+      package = pkgs.posy-cursors;
+      name = "Posy_Cursor_Mono_Black";
+      size = 24;
+    };
+    polarity = "dark";
+    image = ./backgrounds/starryboat.jpg;
   };
-  stylix.polarity = "dark";
-  stylix.image = ./backgrounds/starryboat.jpg;
 }
