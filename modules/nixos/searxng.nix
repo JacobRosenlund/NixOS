21d12586200571da78c9378f91e399401f0b39b7
JacobRosenlund <jakemrosenlund@gmail.com>
2026-04-24 15:25:33 -0600

    Updated searxng

--- modules/nixos/searxng.nix
+++ modules/nixos/searxng.nix
@@ -5,9 +5,25 @@
     enable = true;
     redisCreateLocally = true;
     environmentFile = "/home/jacobr/.searxng.env";
-    settings.server = {
-      bind_address = "127.0.0.1";
-      port = 2050;
+
+    settings = {
+      server = {
+        bind_address = "127.0.0.1";
+        port = 2050;
+      };
+
+      engines = [
+        {
+          name = "nixos-packages";
+          engine = "xpath";
+          paging = false;
+          search_url = "https://search.nixos.org/packages?query={query}";
+          url_xpath = "//ul/li/[@class=\"package\"]";
+          title_xpath = "";
+          content_xpath = "";
+          shortcut = "nix";
+        }
+      ];
     };
   };
 }
