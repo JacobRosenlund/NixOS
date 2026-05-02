49f7938b3282f70cac5f5ba8eb082414e75f5dee
JacobRosenlund <jakemrosenlund@gmail.com>
2026-05-02 11:12:53 -0600

    Fixed the location and configuration of radion.nix

--- modules/packages/system-packages/radio.nix
+++ modules/packages/system-packages/radio.nix
@@ -1,8 +1,11 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
+  hardware.rtl-sdr.enable = true;
+  users.users.jacobr.extraGroups = [ "plugdev" ];
+
   environment.systemPackages = with pkgs; [
-    # sdrpp
-    # chirp
+    sdrpp
+    chirp
   ];
 }
--- modules/packages/user-packages/radio.nix
+++
@@ -1,8 +0,0 @@
-{ config, lib, pkgs, inputs, ... }:
-
-{
-  hardware.rtl-sdr.enable = true;
-  users.users.jacobr.extraGroups = [ "plugdev" ];
-
-  home.packages = with.pkgs; [ sdrpp ];
-}
