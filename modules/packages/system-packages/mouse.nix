04728d795edc8aa8b94ca0ed343285b3fb6f0ee1
JacobRosenlund <jakemrosenlund@gmail.com>
2026-05-30 20:05:16 -0800

    Fixed spelling

--- modules/packages/system-packages/mouse.nix
+++ modules/packages/system-packages/mouse.nix
@@ -2,6 +2,6 @@
 
 {
   hardware.openrazer.enable = true;
-  environent.systemPackages = with pkgs; [ openrazer-daemon polychromatic ];
+  environment.systemPackages = with pkgs; [ openrazer-daemon polychromatic ];
   users.user.jacobr = { extraGroups = [ "openrazer" ]; };
 }
